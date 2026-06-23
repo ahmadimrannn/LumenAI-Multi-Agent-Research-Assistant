13ac55bb4de4248a6a0ab8fbbb62ad40c4b0344a
Ahmad Imran <ahmadimran67208@gmail.com>
2026-06-23 16:46:18 +0500

    Change the deprecated langchain package for tavily search

--- backend/agents/researcher.py
+++ backend/agents/researcher.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 from langchain_core.messages import AIMessage
 from .agents_state import AgentsState
-from langchain_community.tools.tavily_search import TavilySearchResults 
+from langchain_tavily import TavilySearch
 
 load_dotenv()
 
@@ -9,10 +9,12 @@ def researcher_agent(state: AgentsState):
   """Gets raw tavily search results"""
 
   query = state['query']
-  tavily = TavilySearchResults(max_results=3)
-  result = tavily.invoke(query)
+  tavily = TavilySearch(max_results=3)
+  response = tavily.invoke({"query": query})
+  result = response['results']
 
   agent_message = f"Here are the search results based on the query {query}. \n\n 🔍 Search Results: {result[:2]}"
+
 
   return {
     "messages": [AIMessage(content=agent_message)],

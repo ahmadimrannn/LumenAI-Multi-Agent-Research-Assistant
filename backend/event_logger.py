c7d0f88d9b316b72e8c46b20e55edc97c9dcbf11
Ahmad Imran <ahmadimran67208@gmail.com>
2026-09-11 16:16:07 +0500

    Update the import order

--- backend/event_logger.py
+++ backend/event_logger.py
@@ -3,10 +3,10 @@ import os
 import traceback
 import psycopg
 from dotenv import load_dotenv
+load_dotenv()
 
 from automated_sentryloop_trigger.sentryloop_trigger import maybe_trigger_investigation
 
-load_dotenv()
 
 DB_DSN = os.getenv("DB_URI_FOR_LOGS")
 
